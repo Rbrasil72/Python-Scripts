3e3cfbf44b8383e852022ef895489db8c0e92fa6
Rodrigo Brasil <108822086+Rbrasil72@users.noreply.github.com>
2024-02-01 15:02:19 -0100

    Update Red Team Project Agressive nmap scan.py

--- Red Team Project Agressive nmap scan.py
+++ Red Team Project Agressive nmap scan.py
@@ -25,7 +25,7 @@ def scan_hosts(ip_list, output_file):
         for ip in ip_list:
             report_file.write(f"Scanning {ip}...\n")
             try:
-                result = subprocess.run(['nmap','-sV','-A','-sV','-v', ip], capture_output=True, text=True, check=True)
+                result = subprocess.run(['nmap','-sV','-A','-O','-v', ip], capture_output=True, text=True, check=True)
                 report_file.write(result.stdout)
             except subprocess.CalledProcessError as e:
                 report_file.write(f"Error scanning {ip}: {e}\n")
